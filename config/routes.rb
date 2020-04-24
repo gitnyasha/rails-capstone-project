0db60087943cd67d066a029b638a893f38c3ad24
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-04-24 20:29:38 +0200

    created user and login session

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  resources :users, only: %i[new show create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
