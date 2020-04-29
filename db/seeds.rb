4d6582a6b9bae8fc4300a3cec2b593e7c0203943
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-04-29 14:53:45 +0200

    final design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  name: "Marshall Chikari",
+  username: "marshallchikari",
+)
+
+User.create(
+  name: "User Two",
+  username: "user2",
+)
+
+Group.create(
+  name: "Food",
+  user_id: 1,
+)
+
+Group.create(
+  name: "Utilities",
+  user_id: 1,
+)
+
+Group.create(
+  name: "Food",
+  user_id: 1,
+)
